2520151f13110197733812d913df49f5036d0b30
marat-elpatron <marat.elpatron@gmail.com>
2026-06-19 20:43:33 +0300

    Интегрирована Prisma и Better Auth

---
+++ server/utils/auth.ts
@@ -0,0 +1,16 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./prisma";
+import { bearer } from "better-auth/plugins";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [
+    bearer()
+  ],
+});
---
+++ server/utils/prisma.ts
@@ -0,0 +1,21 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+import { PrismaClient } from "../generated/prisma";
+
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({
+  connectionString,
+  max: 100,
+  min: 10,
+  statement_timeout: 10000,
+  connectionTimeoutMillis: 5000,
+  idleTimeoutMillis: 15000,
+});
+const adapter = new PrismaPg(pool);
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
